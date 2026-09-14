4f73a7590dd69c7122ed2b8b9d0b497dc35049a1
littl <littl@CHRIS-PC>
2026-09-14 17:37:31 -0400

    firstPush

---
+++ src/lib.rs
@@ -0,0 +1,24 @@
+use wasm_bindgen::prelude::*;
+use substring::Substring;
+
+
+#[wasm_bindgen]
+pub fn extract_video_id(input: &str) -> String {
+	if let Some(idx) = input.find("watch?v=") {
+        return input.substring(idx + 8, input.chars().count())
+            .split('&').next().unwrap_or("").to_string();
+    }
+    if let Some(idx) = input.find("youtu.be/") {
+        return input.substring(idx + 9, input.chars().count())
+            .split('?').next().unwrap_or("").to_string();
+    }
+    if let Some(idx) = input.find("embed/") {
+        return input.substring(idx + 6, input.chars().count())
+            .split('?').next().unwrap_or("").to_string();
+    }
+
+    String::new()
+
+
+
+}
